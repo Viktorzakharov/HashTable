83944e73b9ad942361a6e5d8f1ce5fc4a8aef5cc
Viktorzakharov <Viktorzakharov93@outlook.com>
2018-11-05 02:02:14 +0500

    версия 3

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -7,23 +7,27 @@ namespace HashTable
         static void Main()
         {
             var table = new HashTable();
-
             table.Put("a");
             table.Put("b");
             table.Put("c");
-            table.Put("d");
-            table.Put("e");
-            table.Put("f");
-            table.Put("g");
-            table.Put("h");
-            table.Put("i");
-            table.Put("j");
 
-            for (int i = 0; i < table.Array.Length; i++)
-                Console.Write(table.Array[i] + " ");
+            var checkTable = new HashTable();
+            checkTable.Put("c");
+            checkTable.Put("d");
+            checkTable.Put("e");
+
+            Write(table.Array);
+            Write(checkTable.Array);
+            Write(table.Intersection(checkTable));
+            Write(table.Union(checkTable));
+            Write(table.Difference(checkTable));
+        }
+
+        public static void Write(string[] array)
+        {
+            foreach (var e in array)
+                Console.Write(e + " ");
             Console.WriteLine();
-
-            Console.WriteLine(table.Find("i"));
         }
     }
 }
